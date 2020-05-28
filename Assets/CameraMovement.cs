cc7e7d17be6a9441de344a4f4266d664e80f6ffa
Patryk Sojkowski <patryk.sojkowski@ericsson.com>
2020-05-28 15:14:35 +0200

    Add project files.

---
+++ Assets/CameraMovement.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+
+    private float x;
+    private float y;
+    private Vector3 rotateValue;
+
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.LeftControl))
+        {
+            y = Input.GetAxis("Mouse X");
+            x = Input.GetAxis("Mouse Y");
+            rotateValue = new Vector3(x, y * -1, 0);
+            transform.eulerAngles = transform.eulerAngles - rotateValue;
+        }
+    }
+}
+
+//      var heading = cameraTransform.rotation;
+//      if(Input.GetKey(KeyCode.W)) // FORWARD
+//      {
+//          cameraTransform.position += new Vector3(heading.x, heading.y, heading.z);
+//      }
+//      if (Input.GetKey(KeyCode.S)) // BACKWARDS
+//      {
+//          cameraTransform.position -= new Vector3(heading.x, heading.y, heading.z);
+//      }
+//      if (Input.GetKey(KeyCode.A)) // LEFT ROTATION
+//      {
+//          cameraTransform.rota
+//      }
+//      if (Input.GetKey(KeyCode.D)) // RIGHT ROTATION
+//      {
+
+//      }
+//      if (Input.GetKey(KeyCode.Q)) // UPWARD ROTATION
+//      {
+
+//      }
+//      if (Input.GetKey(KeyCode.E)) // DOWNWARD ROTATION
+//      {
+
+//      }
---
+++ Assets/EarthMovement.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System;
+
+public class EarthMovement : MonoBehaviour {
+    private double t = 0;
+    public double radius = 100d;
+
+    private void Start()
+    {
+        var t1 = Math.Acos(transform.position.x / radius);
+        var t2 = Math.Acos(transform.position.z / radius);
+        if(Math.Abs(t1 - t2) <= double.Epsilon)
+        {
+            t = t1;
+            Debug.Log("Condition fullfiled. t =" + t.ToString());
+        }
+    }
+
+    void FixedUpdate () {
+        t += Math.PI / 18000;
+        t %= 2 * Math.PI;
+        transform.position = new Vector3((float)(radius*Math.Cos(t)), 0f, (float)(radius*Math.Sin(t)));
+	}
+}
